5089e4f0539948fa47e23e16a0b72d28479863a8
kriper2OO3 <sp10072OO3@mail.ru>
2025-03-22 15:09:35 +0500

    добавлен палиндром и калькулятор

---
+++ calculator.py
@@ -0,0 +1,12 @@
+
+def calc():
+    op = input("Выберите операцию (+, -, *, /): ")
+    a, b = float(input("Первое число: ")), float(input("Второе число: "))
+
+    if (op == '+'): res = a + b
+    elif (op == '-'): res = a - b
+    
+    return res
+
+result = calc()
+print(f"Результат операции: {result}")
---
+++ palindrome.py
@@ -0,0 +1,8 @@
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+user_input = input("Введите строку: ")
+
+if is_palindrome(user_input):
+    print("Да, это палиндром.")
