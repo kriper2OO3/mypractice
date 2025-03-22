5b8821a0e66df99692f96817a27be968c0a91d4a
kriper2OO3 <sp10072OO3@mail.ru>
2025-03-22 15:43:26 +0500

    доавлен fibonacci и добавлены 2 операции в калькулятор

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+n = int(input("Введите номер числа Фибоначчи: "))
+print(f"Число Фибоначчи под номером {n} равно {fibonacci(n)}")
